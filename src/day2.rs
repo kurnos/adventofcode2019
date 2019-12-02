af217a38a9bd8572ce638690b50bff5fa36eeba1
Johan Olsson <kurnos@gmail.com>
2019-12-02 12:22:23 +0100

    Refactor day2

--- src/day2.rs
+++ src/day2.rs
@@ -7,13 +7,13 @@ pub fn first(content: &String) -> u32 {
 
 pub fn second(content: &String) -> u32 {
     let initial = parse_memory(content);
-    for a in 0..100 {
-        for b in 0..100 {
+    for noun in 0..100 {
+        for verb in 0..100 {
             let mut memory = initial.clone();
-            memory[1] = a;
-            memory[2] = b;
+            memory[1] = noun;
+            memory[2] = verb;
             if run(memory) == 19690720 {
-                return 100 * a + b;
+                return 100 * noun + verb;
             }
         }
     }
@@ -29,24 +29,25 @@ fn parse_memory(content: &String) -> Vec<u32> {
 }
 
 fn run(mut memory: Vec<u32>) -> u32 {
-    let mut pc = 0usize;
-    while asdf(pc, &mut memory) {
-        pc += 4;
-    }
+    std::iter::successors(Some(0usize), |&ic| run_instruction(ic, &mut memory)).last();
     memory[0]
 }
 
-fn asdf(pc: usize, program: &mut [u32]) -> bool {
-    if program[pc] == 99 {
-        return false;
-    } else if program[pc] == 1 {
-        program[program[pc + 3] as usize] =
-            program[program[pc + 1] as usize] + program[program[pc + 2] as usize];
-    } else if program[pc] == 2 {
-        program[program[pc + 3] as usize] =
-            program[program[pc + 1] as usize] * program[program[pc + 2] as usize];
-    } else {
-        panic!(format!("unknown command {}", program[pc]));
+fn run_instruction(ic: usize, memory: &mut [u32]) -> Option<usize> {
+    match memory[ic] as usize {
+        99 => {
+            None
+        },
+        1 => {
+            memory[memory[ic + 3] as usize] =
+            memory[memory[ic + 1] as usize] + memory[memory[ic + 2] as usize];
+            Some(ic + 4)
+        },
+        2 => {
+            memory[memory[ic + 3] as usize] =
+            memory[memory[ic + 1] as usize] * memory[memory[ic + 2] as usize];
+            Some(ic + 4)
+        },
+        opcode => panic!(format!("unknown command {}", memory[opcode]))
     }
-    true
 }
