9bbb54b54a735c72d9d677d359f5d856d7077594
Johan Olsson <kurnos@gmail.com>
2019-12-04 13:22:35 +0100

    Refactor day4

--- src/day4.rs
+++ src/day4.rs
@@ -2,21 +2,27 @@ pub fn first(low: u32, high: u32) -> u32 {
     (low..high).filter(|&x| is_valid1(x)).count() as u32
 }
 
-fn is_valid1(mut n: u32) -> bool {
+fn decending_counts(mut n: u32) -> Option<[u8; 10]> {
     let mut counts: [u8; 10] = [0; 10];
     let mut last = 9;
     for _ in 0..6 {
         let d = n % 10;
         if d > last {
-            return false;
+            return None;
         }
         last = d;
         counts[d as usize] += 1;
         n = n / 10;
     }
-    for &c in &counts {
-        if c >= 2 {
-            return true;
+    Some(counts)
+}
+
+fn is_valid1(n: u32) -> bool {
+    if let Some(counts) = decending_counts(n) {
+        for &c in &counts {
+            if c >= 2 {
+                return true;
+            }
         }
     }
     false
@@ -26,21 +32,12 @@ pub fn second(low: u32, high: u32) -> u32 {
     (low..high).filter(|&x| is_valid2(x)).count() as u32
 }
 
-fn is_valid2(mut n: u32) -> bool {
-    let mut counts: [u8; 10] = [0; 10];
-    let mut last = 9;
-    for _ in 0..6 {
-        let d = n % 10;
-        if d > last {
-            return false;
-        }
-        last = d;
-        counts[d as usize] += 1;
-        n = n / 10;
-    }
-    for &c in &counts {
-        if c == 2 {
-            return true;
+fn is_valid2(n: u32) -> bool {
+    if let Some(counts) = decending_counts(n) {
+        for &c in &counts {
+            if c == 2 {
+                return true;
+            }
         }
     }
     false
