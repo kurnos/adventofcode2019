654f9e015ef31ac397c30421587fd7ac1e48df3c
Johan Olsson <kurnos@gmail.com>
2019-12-05 10:57:02 +0100

    Parallelize day2

--- src/day2.rs
+++ src/day2.rs
@@ -1,4 +1,5 @@
 use crate::computer::{parse_memory, run};
+use rayon::prelude::*;
 
 pub fn first(content: &String) -> i32 {
     let mut memory = parse_memory(content);
@@ -9,15 +10,10 @@ pub fn first(content: &String) -> i32 {
 
 pub fn second(content: &String) -> i32 {
     let initial = parse_memory(content);
-    for noun in 0..100 {
-        for verb in 0..100 {
-            let mut memory = initial.clone();
-            memory[1] = noun;
-            memory[2] = verb;
-            if run(memory, vec![]).memory[0] == 19690720 {
-                return 100 * noun + verb;
-            }
-        }
-    }
-    0
+    (0i32..10000).into_par_iter().find_first(|i| {
+        let mut memory = initial.clone();
+        memory[1] = i / 100;
+        memory[2] = i % 100;
+        run(memory, vec![]).memory[0] == 19690720
+    }).unwrap()
 }
