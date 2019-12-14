ffc428e315e2681555865e8a450a425a8bdeb54d
Johan Olsson <kurnos@gmail.com>
2019-12-14 14:56:39 +0100

    Sensible defaults for template

--- src/day0.rs
+++ src/day0.rs
@@ -2,14 +2,12 @@ use crate::infra::Problem;
 
 pub struct DayX;
 
-impl Problem<String, String, i32, i32> for DayX {
+impl Problem<String, String, (), ()> for DayX {
     fn day() -> u8 {
-        unimplemented!();
+        0;
     }
-    fn first(contents: String) -> i32 {
-        unimplemented!();
+    fn first(contents: String) -> () {
     }
-    fn second(contents: String) -> i32 {
-        unimplemented!();
+    fn second(contents: String) -> () {
     }
 }
