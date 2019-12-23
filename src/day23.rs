baab2ba3b7a2cc2adb9448c27d91ccd258d0afe2
Johan Olsson <kurnos@gmail.com>
2019-12-23 08:49:45 +0100

    Make day23 nicer

--- src/day23.rs
+++ src/day23.rs
@@ -10,69 +10,50 @@ impl Problem<String, String, i64, i64> for Day23 {
         23
     }
     fn first(contents: String) -> i64 {
-        run_network(&contents, |_, m| m.is_some()).unwrap().1
+        run_network(&contents, |_, _| true)
     }
     fn second(contents: String) -> i64 {
-        let mut last_y = None;
-        run_network(&contents, |idle, m| match (idle, m, last_y) {
-            (true, Some((_, y)), Some(last)) if last == y => true,
-            (true, Some((_, y)), _) => {
-                last_y = Some(y);
-                false
-            }
-            _ => false,
-        })
-        .unwrap()
-        .1
+        run_network(&contents, |a, b| a == b)
     }
 }
 
-fn run_network<F: FnMut(bool, Option<(i64, i64)>) -> bool>(
+fn run_network<F: FnMut(Option<(i64, i64)>, Option<(i64, i64)>) -> bool>(
     contents: &str,
     mut is_done: F,
-) -> Option<(i64, i64)> {
+) -> i64 {
     let mem = parse_memory(contents);
     let mut cpus = Vec::new();
-    let mut queues = Vec::new();
+    let mut queue = VecDeque::new();
     for i in 0..50 {
         let mut c = Computer::<i64>::from_memory(mem.clone());
         c.run();
-        c.run_with_input(i);
+        tx_rx(&mut c, i, -1, &mut queue);
         cpus.push(c);
-        queues.push(VecDeque::<(i64, i64)>::new());
     }
 
     let mut nat_msg = None;
+    let mut last_nat = None;
     loop {
-        let mut idle = true;
-        for (i, mut c) in cpus.iter_mut().enumerate() {
-            match (i, c.state, queues[i].get(0).cloned()) {
-                (_, ComputerState::WaitingForInput, None) => {
-                    c.run_with_input(-1);
-                }
-                (i, ComputerState::WaitingForInput, Some((x, y))) => {
-                    c.run_with_input(x);
-                    c.run_with_input(y);
-                    queues[i].pop_front();
-                    idle = false;
-                }
-                (_, ComputerState::HasOutput(_), _) => {
-                    let (d, x, y) = c.next_tuple().unwrap();
-                    if d == 255 {
-                        nat_msg = Some((x, y));
-                    } else {
-                        queues[d as usize].push_back((x, y));
-                    }
-                    idle = false;
-                }
-                _ => {}
+        while let Some((d, x, y)) = queue.pop_front() {
+            if d == 255 {
+                nat_msg = Some((x, y));
+            } else {
+                tx_rx(&mut cpus[d as usize], x, y, &mut queue);
             }
         }
-        if is_done(idle, nat_msg) {
-            return nat_msg;
+        let (x, y) = nat_msg.unwrap();
+        if is_done(last_nat, nat_msg) {
+            return y;
         }
-        if let (true, Some((x, y))) = (idle, nat_msg) {
-            queues[0].push_back((x, y));
-        }
+        tx_rx(&mut cpus[0], x, y, &mut queue);
+        last_nat = nat_msg;
+    }
+}
+
+fn tx_rx(mut c: &mut Computer<i64>, x: i64, y: i64, target: &mut VecDeque<(i64, i64, i64)>) {
+    c.run_with_input(x);
+    c.run_with_input(y);
+    while let ComputerState::HasOutput(_) = c.state {
+        target.push_back(c.next_tuple().unwrap());
     }
 }
